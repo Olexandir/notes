7275adbf6e8d59b38d5f65f99f25b92875d340ec
Olexandir <gypsyman534@gmail.com>
2023-11-24 12:24:47 +0100

    init pure app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
+  standalone: true,
+  imports: [CommonModule],
 })
-export class AppComponent {
-  title = 'code-notes';
-}
+export class AppComponent {}
