6271a5d2a658d5d666999f30435d5acf00e960ea
Olexandir <gypsyman534@gmail.com>
2023-11-26 15:39:28 +0300

    init branch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
+import { GetAllService } from "./services/getAll.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
-  standalone: true,
-  imports: [CommonModule],
 })
-export class AppComponent {}
+export class AppComponent {
+  constructor(private getServ: GetAllService) { }
+
+  ngOnInit() {
+    this.getServ.getAllCharacters().subscribe(data => console.log(data))
+  }
+}
---
+++ src/app/app.routing.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from "@angular/core";
+
+const routes = [];
+
+@NgModule({})
+
+export class AppRoutingModule {}
