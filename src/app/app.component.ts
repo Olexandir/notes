d200ee3c6cfe49581eb74804cb16a7731d4b9173
Olexandir <gypsyman534@gmail.com>
2023-11-27 08:10:00 +0100

    restore

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,8 @@
-import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.scss"],
-  standalone: true,
-  imports: [CommonModule],
 })
 export class AppComponent {}
